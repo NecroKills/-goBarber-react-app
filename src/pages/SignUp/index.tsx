3eb5a40b1263d099674e39a069978d2614631f6e
Maycon <maycondasilvamoreira@hotmail.com>
2020-05-21 19:59:49 -0300

    Foi instalando o axios e feito a parte de cadastro do usuario.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -19,6 +19,7 @@ import * as Yup from 'yup';
 import getValidationErrors from '../../utils/getValidationErrors';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import api from '../../services/api';
 
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
@@ -54,9 +55,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.',
+      );
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
