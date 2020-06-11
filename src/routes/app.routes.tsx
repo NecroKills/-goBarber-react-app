7c219b4f4de901db87341448348f13585751e3d5
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-11 14:23:52 -0300

    Foi criado todas as rotas e todas as paginas da aplicação, Tambem foi adicionado o ip que esta rodando minha api para conseguir conectar o celular normalmente.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
@@ -15,6 +18,10 @@ const AuthRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
