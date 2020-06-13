66e8555ebfdec5fcecf127c7ad9aac93ab7b17cc
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-13 12:20:02 -0300

    Foi modificado para ao clicar na foto ser direcionado para a pagina de perfil. Foi criado a estrutura da pagina de atualizaçao de perfil. Foi criado os componentes:  Container, Title, UserAvatarButton, UserAvatar, BackButton e seus estilos.

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,10 +1,204 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useRef } from 'react';
 
-import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
 
-const Profile: React.FC = () => {
-  return <View />;
+import { FormHandles } from '@unform/core';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import api from '../../services/api';
+
+import {
+  Container,
+  Title,
+  UserAvatarButton,
+  UserAvatar,
+  BackButton,
+} from './styles';
+
+import { useAuth } from '../../hooks/auth';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const { user } = useAuth();
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation();
+
+  const emailInputRef = useRef<TextInput>(null);
+  const oldPasswordInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const confirmPasswordInputRef = useRef<TextInput>(null);
+
+  // Recebe os dados do formulario'.
+  const handleSignUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail Valido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await api.post('/users', data);
+
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.',
+      );
+
+      navigation.goBack();
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert(
+        'Erro no cadastro',
+        'Ocorreu um erro ao fazer cadastro. Tente novamente.',
+      );
+    }
+  }, []);
+
+  const handleGoBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <BackButton
+              onPress={() => {
+                handleGoBack;
+              }}
+            >
+              <Icon name="chevron-left" size={24} color="#999591" />
+            </BackButton>
+
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar
+                source={{
+                  uri:
+                    user.avatar_url ||
+                    'https://api.adorable.io/avatars/186/abott@adorable.io.png',
+                }}
+              />
+            </UserAvatarButton>
+            <View>
+              <Title>Meu Perfil</Title>
+            </View>
+            <Form ref={formRef} onSubmit={handleSignUp}>
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={emailInputRef}
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  oldPasswordInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={oldPasswordInputRef}
+                secureTextEntry
+                name="old_password"
+                icon="lock"
+                placeholder="Senha atual"
+                textContentType="newPassword"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                name="password"
+                icon="lock"
+                placeholder="Nova senha"
+                textContentType="newPassword"
+                returnKeyType="next"
+                containerStyle={{ marginTop: 16 }}
+                onSubmitEditing={() => {
+                  confirmPasswordInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={confirmPasswordInputRef}
+                secureTextEntry
+                name="password_confirmation"
+                icon="lock"
+                placeholder="Confirmar senha"
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Confirmar mudanças
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
 
-export default Profile;
+export default SignUp;
